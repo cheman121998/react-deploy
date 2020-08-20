d1d802be78f7009f40b9812f8b3cd7dc9d457553
Man Man <chethiminhman121998@gmail.com>
2020-08-20 14:45:10 +0700

    Updates

---
+++ precache-manifest.11f9a4f2d12f153010aa1e2c898a1585.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ff2d9fcde22814608dce36f8a738a02",
+    "url": "/github.io/react-deploy/index.html"
+  },
+  {
+    "revision": "e9f4406e9d359239b224",
+    "url": "/github.io/react-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6a10a78df6c76ca3a627",
+    "url": "/github.io/react-deploy/static/js/2.b1b31c97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/react-deploy/static/js/2.b1b31c97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9f4406e9d359239b224",
+    "url": "/github.io/react-deploy/static/js/main.a76d72f2.chunk.js"
+  },
+  {
+    "revision": "1b4adbd71ba92cbb68a3",
+    "url": "/github.io/react-deploy/static/js/runtime-main.5828094d.js"
+  }
+]);
